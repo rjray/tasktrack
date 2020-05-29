f63162bf0755f8d0854415cdf1b83e4d7d40f3bc
Randy J. Ray <rjray@blackperl.com>
2020-05-29 13:16:47 -0700

    Basic set-up of client. Incomplete.

--- client/main.jsx
+++ client/main.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
-import { Meteor } from 'meteor/meteor';
-import { render } from 'react-dom';
-import { App } from '/imports/ui/App';
+import React from "react";
+import { Meteor } from "meteor/meteor";
+import { render } from "react-dom";
+
+import "../imports/startup/accounts-config.js";
+import App from "../imports/ui/App.jsx";
+
+import "bootstrap/dist/css/bootstrap-reboot.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 Meteor.startup(() => {
-  render(<App/>, document.getElementById('react-target'));
+  render(<App />, document.getElementById("react-target"));
 });
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
+import React from "react";
+import { Helmet } from "react-helmet";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { LinkContainer } from "react-router-bootstrap";
+import { Meteor } from "meteor/meteor";
+import { withTracker } from "meteor/react-meteor-data";
 
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
-  </div>
+import Tasks from "../api/tasks";
+import AccountsUIWrapper from "./AccountsUIWrapper";
+
+const App = (props) => (
+  <Router>
+    <Helmet titleTemplate="TaskTrack - %s">
+      <title>Home</title>
+    </Helmet>
+    <Container fluid>
+      <Navbar bg="light" expand="sm">
+        <LinkContainer to="/">
+          <Navbar.Brand>TaskTrack</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
+        <Nav>
+          <AccountsUIWrapper />
+        </Nav>
+      </Navbar>
+    </Container>
+    <Container fluid>
+      <Switch>
+        <Route exact path="/">
+          <Container></Container>
+        </Route>
+      </Switch>
+    </Container>
+  </Router>
 );
+
+export default withTracker(() => {
+  Meteor.subscribe("tasks");
+
+  return {
+    tasks: Tasks.find({}).fetch(),
+    currentUser: Meteor.user(),
+  };
+})(App);
