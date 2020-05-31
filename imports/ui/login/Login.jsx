4c36a628afca43c7515aa4ddbbab4387c389c97a
Randy J. Ray <rjray@blackperl.com>
2020-05-31 13:24:17 -0700

    Basic login page. Not finished.

---
+++ imports/ui/login/Login.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import { Redirect } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import { Meteor } from "meteor/meteor";
+
+const Login = () => {
+  return Meteor.user() ? (
+    <Redirect to={{ pathname: "/" }} />
+  ) : (
+    <>
+      <Helmet>
+        <title>Log In</title>
+      </Helmet>
+      <Container>
+        <h1 className="text-center mt-3">
+          Welcome to Task<em>Track</em>
+        </h1>
+        <p className="text-center mt-5">
+          You need to log in or create an account to use this tool.
+        </p>
+      </Container>
+    </>
+  );
+};
+
+export default Login;
