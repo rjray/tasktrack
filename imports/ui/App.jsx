954217607a69135356b79f8f3de2f9497f7f7df7
Randy J. Ray <rjray@blackperl.com>
2020-05-31 13:24:03 -0700

    Added back popup menu and added to routing.

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -3,6 +3,7 @@ import { Helmet } from "react-helmet";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import Nav from "react-bootstrap/Nav";
 import { LinkContainer } from "react-router-bootstrap";
 import { Meteor } from "meteor/meteor";
@@ -10,6 +11,7 @@ import { withTracker } from "meteor/react-meteor-data";
 
 import Tasks from "../api/tasks";
 import AccountsUIWrapper from "./AccountsUIWrapper";
+import Login from "./login/Login";
 
 const App = (props) => (
   <Router>
@@ -19,10 +21,26 @@ const App = (props) => (
     <Container fluid>
       <Navbar bg="light" expand="sm">
         <LinkContainer to="/">
-          <Navbar.Brand>TaskTrack</Navbar.Brand>
+          <Navbar.Brand>
+            Task<em>Track</em>
+          </Navbar.Brand>
         </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <NavDropdown title="View" id="basic-nav-dropdown">
+              <LinkContainer to="/">
+                <NavDropdown.Item>My Tasks</NavDropdown.Item>
+              </LinkContainer>
+              <LinkContainer to="/all">
+                <NavDropdown.Item>All Tasks</NavDropdown.Item>
+              </LinkContainer>
+              <LinkContainer to="/new">
+                <NavDropdown.Item>New Task</NavDropdown.Item>
+              </LinkContainer>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
         <Nav>
           <AccountsUIWrapper />
         </Nav>
@@ -32,6 +50,15 @@ const App = (props) => (
       <Switch>
         <Route exact path="/">
           <Container></Container>
+        </Route>
+        <Route path="/all">
+          <Container></Container>
+        </Route>
+        <Route path="/new">
+          <Container></Container>
+        </Route>
+        <Route path="/login">
+          <Login />
         </Route>
       </Switch>
     </Container>
