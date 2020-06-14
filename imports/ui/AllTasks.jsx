74c5208e9feefec8334687282f94637459aa72b3
Randy J. Ray <rjray@blackperl.com>
2020-06-14 14:08:29 -0700

    Buttons and spacing for actions.

--- imports/ui/AllTasks.jsx
+++ imports/ui/AllTasks.jsx
@@ -3,6 +3,7 @@ import { Helmet } from "react-helmet";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 const NoTasks = () => <p>There are currently no tasks.</p>;
 
@@ -14,11 +15,18 @@ const AllTasks = ({ tasks, currentUser }) => {
       </Helmet>
       <Container>
         <Row>
-          <Col xs={12} className="mt-3 text-center">
-            <span className="h1">All Tasks</span>{" "}
-            <sup>
+          <Col xs={4} md={2} className="align-top text-left">
+            <Button>New Task</Button>
+          </Col>
+          <Col xs={4} md={10} className="mt-3 text-center">
+            <span className="h1">All Tasks</span>
+            <br />
+            <span>
               (<a href="/">your tasks</a>)
-            </sup>
+            </span>
+          </Col>
+          <Col xs={4} md={2} className="align-top text-right">
+            <Button>Show Finished</Button>
           </Col>
         </Row>
         <Row>{tasks.length === 0 && <NoTasks />}</Row>
--- imports/ui/Home.jsx
+++ imports/ui/Home.jsx
@@ -3,6 +3,7 @@ import { Helmet } from "react-helmet";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 const NoTasks = () => <p>You have no tasks.</p>;
 
@@ -14,11 +15,18 @@ const Home = ({ tasks }) => {
       </Helmet>
       <Container>
         <Row>
-          <Col xs={12} className="mt-3 text-center">
-            <span className="h1">Your Tasks</span>{" "}
-            <sup>
+          <Col xs={4} md={2} className="align-top text-left">
+            <Button>New Task</Button>
+          </Col>
+          <Col xs={4} md={8} className="mt-3 text-center align-top">
+            <span className="h1">Your Tasks</span>
+            <br />
+            <span>
               (<a href="/all">all tasks</a>)
-            </sup>
+            </span>
+          </Col>
+          <Col xs={4} md={2} className="align-top text-right">
+            <Button>Show Finished</Button>
           </Col>
         </Row>
         <Row>{tasks.length === 0 && <NoTasks />}</Row>
