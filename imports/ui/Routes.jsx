7f0fa664aed6f8aecab88592315ed0be53e23fac
Randy J. Ray <rjray@blackperl.com>
2020-06-14 17:07:48 -0700

    Remove refs to NewTask.jsx

--- imports/ui/Routes.jsx
+++ imports/ui/Routes.jsx
@@ -3,7 +3,6 @@ import { Route, Switch } from "react-router-dom";
 
 import Home from "./Home";
 import AllTasks from "./AllTasks";
-import NewTask from "./NewTask";
 
 const Routes = (props) => (
   <Switch>
@@ -12,9 +11,6 @@ const Routes = (props) => (
     </Route>
     <Route path="/all">
       <AllTasks {...props} />
-    </Route>
-    <Route path="/new">
-      <NewTask {...props} />
     </Route>
   </Switch>
 );
