8a6a93fe47d04ddf60881932389f0c7ff7e71b30
Randy J. Ray <rjray@blackperl.com>
2020-06-26 20:11:21 -0700

    Expandable rows.

--- imports/ui/TaskTable.jsx
+++ imports/ui/TaskTable.jsx
@@ -1,8 +1,27 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 import DataTable from "react-data-table-component";
 import format from "date-fns/format";
 
 import { taskPriorities, taskStatusList } from "../api/tasks";
+
+const TaskExpand = ({ data: task }) => {
+  return (
+    <Container fluid className="mt-2 mb-3">
+      <Row>
+        <Col xs={6}>
+          <Button>Edit</Button>
+        </Col>
+        <Col xs={6} className="text-right">
+          <Button>Delete</Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 const columns = [
   {
@@ -23,6 +42,13 @@ const columns = [
     cell: (row) => taskStatusList[row.status],
   },
   {
+    name: <b>Created</b>,
+    selector: "createdAt",
+    sortable: true,
+    hide: "md",
+    cell: (row) => format(row.createdAt, "E, MMM do yyyy, h:mma"),
+  },
+  {
     name: <b>Due</b>,
     selector: "dueAt",
     sortable: true,
@@ -32,21 +58,29 @@ const columns = [
 
 const TaskTable = ({ tasks }) => {
   const pagination =
-    tasks.length < 26
+    tasks.length < 11
       ? {}
       : {
           pagination: true,
-          paginationPerPage: 25,
+          paginationPerPage: 10,
         };
 
   return (
     <DataTable
-      striped
       responsive
+      dense
+      noDataComponent={<p>No tasks to display.</p>}
+      keyField="_id"
+      defaultSortField="dueAt"
+      defaultSortAsc={false}
       highlightOnHover
       pointerOnHover
       data={tasks}
       columns={columns}
+      expandableRows
+      expandOnRowClicked
+      expandableRowsHideExpander
+      expandableRowsComponent={<TaskExpand />}
       {...pagination}
     />
   );
